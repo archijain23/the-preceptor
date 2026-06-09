f017b101bff525362214049270bd4adaf00c6f3d
Archi Jain <jainarchi023@gmail.com>
2026-06-10 02:20:52 +0530

    fix: remove @tanstack/react-query from main.jsx (Fix 1)

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
-
-const queryClient = new QueryClient();
 
 const rootEl = document.getElementById("root");
 if (rootEl) {
   createRoot(rootEl).render(
     <React.StrictMode>
       <HelmetProvider>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </QueryClientProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </HelmetProvider>
     </React.StrictMode>,
   );
