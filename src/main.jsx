69f1534b1989a874f9a1c82c21245b91db0fb813
Archi Jain <jainarchi023@gmail.com>
2026-06-10 02:26:55 +0530

    fix: import styles.css in main.jsx so CSS loads

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import "./styles.css";
 import App from "./App";
 
 const rootEl = document.getElementById("root");
