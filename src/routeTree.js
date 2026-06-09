efef4b5ecafa74a1084297234a6719cd4f26cd85
Archi Jain <jainarchi023@gmail.com>
2026-06-09 23:52:46 +0530

    fix: remove router-vite-plugin, use manual static routeTree; fix Tailwind import

--- src/routeTree.js
+++ src/routeTree.js
@@ -14,12 +14,12 @@ import TestimonialsPage from './routes/testimonials.jsx';
 
 const rootRoute = createRootRoute({ component: RootLayout });
 
-const indexRoute = createRoute({ getParentRoute: () => rootRoute, path: '/', component: HomePage });
-const aboutRoute = createRoute({ getParentRoute: () => rootRoute, path: '/about', component: AboutPage });
-const servicesRoute = createRoute({ getParentRoute: () => rootRoute, path: '/services', component: ServicesPage });
-const bookRoute = createRoute({ getParentRoute: () => rootRoute, path: '/book', component: BookPage });
-const contactRoute = createRoute({ getParentRoute: () => rootRoute, path: '/contact', component: ContactPage });
-const shopRoute = createRoute({ getParentRoute: () => rootRoute, path: '/shop', component: ShopPage });
+const indexRoute      = createRoute({ getParentRoute: () => rootRoute, path: '/',             component: HomePage });
+const aboutRoute      = createRoute({ getParentRoute: () => rootRoute, path: '/about',        component: AboutPage });
+const servicesRoute   = createRoute({ getParentRoute: () => rootRoute, path: '/services',     component: ServicesPage });
+const bookRoute       = createRoute({ getParentRoute: () => rootRoute, path: '/book',         component: BookPage });
+const contactRoute    = createRoute({ getParentRoute: () => rootRoute, path: '/contact',      component: ContactPage });
+const shopRoute       = createRoute({ getParentRoute: () => rootRoute, path: '/shop',         component: ShopPage });
 const testimonialsRoute = createRoute({ getParentRoute: () => rootRoute, path: '/testimonials', component: TestimonialsPage });
 
 export const routeTree = rootRoute.addChildren([
