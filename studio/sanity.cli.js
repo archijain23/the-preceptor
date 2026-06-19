1972c4fe2f75d2ba12af8302f17ee414763ed99a
Ayush Tiwari <ayushtiwari102003@gmail.com>
2026-06-19 18:06:34 +0530

    Fix

--- studio/sanity.cli.js
+++
@@ -1,9 +0,0 @@
-import { defineCliConfig } from 'sanity/cli';
-
-export default defineCliConfig({
-  api: {
-    projectId: '9w7fo0ix',
-    dataset: 'production',
-  },
-  studioHost: 'thepreceptor',
-});
